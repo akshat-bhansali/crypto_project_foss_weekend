b7f0e89803f887f9f5dfd5dd6344f33997ef6c40
mohammedasimahmed <asim60999@gmail.com>
2024-03-14 14:45:13 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import { Route,Routes } from 'react-router-dom'
 import Home from './Home'
-import MentalHealth from './MentalHealth'
-import Header from './Header'
 
 const App = () => {
   return (
     <>
-    <Header />
+    
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/mentalHealth' element={<MentalHealth />} />
     </Routes>
     </>
   )
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,12 @@
 import React, { useRef, useEffect } from "react";
 import "./App.css";
-import bgVideo from "./videos/bgVideo4.mp4";
-import avtar2 from "./videos/avtar2.mp4";
-import v3 from "./videos/v3.mp4";
-import avtar2_unscreen from "./videos/avtar2_unscreen.gif";
+import bgVideo from "./assets/heroSectionVideo.mp4";
+import avtar2 from "./assets/avtar2.mp4";
+import metaverse from "./assets/metaverse.mp4";
+import crypto from "./assets/‏cryptocurrency.jpg";
 import { useAnimation, motion, useInView } from "framer-motion";
 import { useNavigate } from "react-router";
+import Navbar from "./Navbar.js";
 
 const Home = () => {
   const textVideoRef = useRef(null);
@@ -15,50 +16,11 @@ const Home = () => {
   const vdScrollRotateRef = useRef(null);
   const contentContainerRef = useRef(null);
   const ref = useRef(null);
-  const ref2 = useRef(null);
 
   const isInView = useInView(ref, { once: false });
-  const isInView2 = useInView(ref2, { once: false });
-  const controls = useAnimation();
-  const controls2 = useAnimation();
   const navigate = useNavigate();
 
-  // const animationVariants = {
-  //   initial: { opacity: 0, y: 50 },
-  //   animate: { opacity: 1, y: 0 },
-  // };
-
   useEffect(() => {
-    const textVideo = textVideoRef.current;
-    const lineAppear = lineAppearRef.current;
-    const lineAppearBottom = lineAppearBottomRef.current;
-    // const lineAppearMiddle = lineAppearMiddleRefs.current;
-    const vdScrollRotate = vdScrollRotateRef.current;
-    const contentContainer = contentContainerRef.current;
-
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 1150) {
-        let lineAppear = document.querySelector(".lineAppear");
-        lineAppear.style.width = "100%";
-        lineAppearBottom.style.width = "100%";
-        let lineAppearMiddle = document.querySelectorAll(".lineAppearMiddle");
-        lineAppearMiddle.forEach((item) => {
-          item.style.height = "85%";
-        });
-        // lineAppearMiddle.forEach((item) => {
-        //   item.style.height = "85%";
-        // });
-      } else if (window.scrollY < 1150 && window.scrollY > 1000) {
-        let lineAppear = document.querySelector(".lineAppear");
-        lineAppear.style.width = "0";
-        lineAppearBottom.style.width = "0";
-        let lineAppearMiddle = document.querySelectorAll(".lineAppearMiddle");
-        lineAppearMiddle.forEach((item) => {
-          item.style.height = "0";
-        });
-      }
-    });
-
     const content = document.querySelector(".content");
     content.style.opacity = "1";
     content.style.transform = "translateY(-70px)";
@@ -66,18 +28,17 @@ const Home = () => {
 
   useEffect(() => {
     if (isInView) {
-      controls.start("visible");
+      const lineAppearBottom = lineAppearBottomRef.current;
+      let lineAppear = document.querySelector(".lineAppear");
+      lineAppear.style.width = "100%";
+      lineAppearBottom.style.width = "100%";
     }
   }, [isInView]);
-  useEffect(() => {
-    if (isInView2) {
-      controls2.start("visible");
-    }
-  }, [isInView2]);
 
   return (
     <div>
-      <video width="1320" height="1100" autoPlay muted loop className="bgVideo">
+      <Navbar />
+      <video autoPlay muted loop className="bgVideo">
         <source src={bgVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
@@ -98,34 +59,19 @@ const Home = () => {
       </div>
       <div className="color">
         <div className="videoContainer">
-          <motion.div
-            ref={ref}
-            variants={{
-              hidden: { opacity: 0, x: -200 },
-              visible: { opacity: 1, x: 0 },
-            }}
-            initial="hidden"
-            animate={controls}
-            transition={{ duration: 1 }}
-          >
-            <div className="textVideo" ref={textVideoRef}>
-              <h2 className="trade">Trade</h2>
-              <h2 className="tradeAsset">
-                The virtual destination for digital assets
-              </h2>
-              <p>
-                Buy and sell LAND, Estates, Avatar wearables and names in the
-                Decentraland Marketplace: stocking the very best digital goods
-                and paraphernalia backed by the Ethereum blockchain.
-              </p>
-              <button className="browse">START BROWSING</button>
-            </div>
-          </motion.div>
-          {/* <video width="720" height="440" autoPlay muted loop>
-            <source src={avtar2} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video> */}
-          <img src={avtar2_unscreen} />
+          <div className="textVideo" ref={textVideoRef}>
+            <h2 className="trade">TRADE</h2>
+            <h2 className="tradeAsset">
+              The virtual destination for digital assets
+            </h2>
+            <p>
+              Buy and sell LAND, Estates, Avatar wearables and names in the
+              Decentraland Marketplace: stocking the very best digital goods and
+              paraphernalia backed by the Ethereum blockchain.
+            </p>
+            <button className="browse">START BROWSING</button>
+          </div>
+          <img src={crypto} className="crypto" />
         </div>
         <div className="scrollRotate">
           <div className="vdRotate">
@@ -136,134 +82,64 @@ const Home = () => {
                 loop
                 id="myVideo"
                 className="vdScrollRotate"
-              >
-                <source src={v3} type="video/mp4" />
-                Your browser does not support HTML5 video.
-              </video>
+                src={metaverse}
+              ></video>
             </div>
           </div>
-          <motion.div
-            ref={ref2}
-            variants={{
-              hidden: { opacity: 0, x: 200 },
-              visible: { opacity: 1, x: 0 },
-            }}
-            initial="hidden"
-            animate={controls2}
-            transition={{ duration: 1 }}
-          >
-            <div className="contentContainer" ref={contentContainerRef}>
-              <h1 className="titleRotate">WORLD</h1>
-              <p className="linePara">
-                The XR Portal To The Open Metaverse Enabling You To Play, Learn,
-                Socialize, Work And Travel To Different Worlds, Making VIVERSE
-                Even More Immersive From The Second You Put On Your Headset.
-              </p>
-              <button className="lineBtn">Explore Now</button>
-            </div>
-          </motion.div>
-        </div>
-      </div>
-      <div className="lineContainer" id="lineContainer">
-        <div className="lineAppear " ref={lineAppearRef}></div>
-        <div className="lineAppearBottom" ref={lineAppearBottomRef}></div>
-        <div className="first a">
-          <div className="lineAppearMiddle" ref={lineAppearMiddleRefs[0]}></div>
-          {/* <h1 className="line">WORLD</h1>
-          <p className="linePara">
-            Explore the diverse worlds within VIVERSE, including exciting
-            collaborations and exhibitions with partners.
-          </p>
-          <button className="lineBtn">Explore Now</button> */}
-          <div className="flip-card">
-            <div className="flip-card-inner">
-              <div className="flip-card-front">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
-                neque similique ea commodi natus excepturi sapiente repellat,
-                sint, hic numquam voluptate tenetur aspernatur voluptates! Vero
-                odit quis ut vel amet?
-              </div>
-              <div className="flip-card-back">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
-                neque similique ea commodi natus excepturi sapiente repellat,
-                sint, hic numquam voluptate tenetur aspernatur voluptates! Vero
-                odit quis ut vel amet?
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="second a">
-          <div className="lineAppearMiddle" ref={lineAppearMiddleRefs[1]}></div>
-          {/* <h1 className="line">WORLD</h1>
-          <p className="linePara" >
-            Explore the diverse worlds within VIVERSE, including exciting
-            collaborations and exhibitions with partners.
-          </p>
-          <button className="lineBtn" >Explore Now</button> */}
-          <div className="flip-card">
-            <div className="flip-card-inner">
-              <div className="flip-card-front">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
-                neque similique ea commodi natus excepturi sapiente repellat,
-                sint, hic numquam voluptate tenetur aspernatur voluptates! Vero
-                odit quis ut vel amet?
-              </div>
-              <div className="flip-card-back">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
-                neque similique ea commodi natus excepturi sapiente repellat,
-                sint, hic numquam voluptate tenetur aspernatur voluptates! Vero
-                odit quis ut vel amet?
-              </div>
-            </div>
-          </div>
-          <div className="lineAppearMiddle" ref={lineAppearMiddleRefs[2]}></div>
-        </div>
-        <div className="third a">
-          {/* <h1 className="line">WORLD</h1>
-          <p className="linePara">
-            Explore the diverse worlds within VIVERSE, including exciting
-            collaborations and exhibitions with partners.
-          </p>
-          <button className="lineBtn">Explore Now</button> */}
-          <div className="flip-card">
-            <div className="flip-card-inner">
-              <div className="flip-card-front">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
-                neque similique ea commodi natus excepturi sapiente repellat,
-                sint, hic numquam voluptate tenetur aspernatur voluptates! Vero
-                odit quis ut vel amet?
-              </div>
-              <div className="flip-card-back">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
-                neque similique ea commodi natus excepturi sapiente repellat,
-                sint, hic numquam voluptate tenetur aspernatur voluptates! Vero
-                odit quis ut vel amet?
-              </div>
-            </div>
+          <div className="contentContainer" ref={contentContainerRef}>
+            <h1 className="titleRotate">WORLD</h1>
+            <p className="linePara">
+              The XR Portal To The Open Metaverse Enabling You To Play, Learn,
+              Socialize, Work And Travel To Different Worlds, Making VIVERSE
+              Even More Immersive From The Second You Put On Your Headset.
+            </p>
+            <button className="lineBtn">Explore Now</button>
           </div>
         </div>
       </div>
-      {/* <div className="container">
-        <div className="flip-card">
-          <div className="flip-card-inner">
-            <div className="flip-card-front">Mohammed Asim Ahmed</div>
+      <motion.div ref={ref} transition={{ duration: 1 }}>
+        <div className="lineContainer" id="lineContainer">
+          <div className="lineAppear " ref={lineAppearRef}></div>
+          <div className="lineAppearBottom" ref={lineAppearBottomRef}></div>
+          <div className="first a">
+            <div
+              className="lineAppearMiddle"
+              ref={lineAppearMiddleRefs[0]}
+            ></div>
 
-            <div className="flip-card-back">Mohammed Asim Ahmed</div>
+            <div className="card">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+              neque similique ea commodi natus excepturi sapiente repellat,
+              sint, hic numquam voluptate tenetur aspernatur voluptates! Vero
+              odit quis ut vel amet?
+            </div>
+          </div>
+          <div className="second a">
+            <div
+              className="lineAppearMiddle"
+              ref={lineAppearMiddleRefs[1]}
+            ></div>
+            <div className="card">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+              neque similique ea commodi natus excepturi sapiente repellat,
+              sint, hic numquam voluptate tenetur aspernatur voluptates! Vero
+              odit quis ut vel amet?
+            </div>
+            <div
+              className="lineAppearMiddle"
+              ref={lineAppearMiddleRefs[2]}
+            ></div>
+          </div>
+          <div className="third a">
+            <div className="card">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+              neque similique ea commodi natus excepturi sapiente repellat,
+              sint, hic numquam voluptate tenetur aspernatur voluptates! Vero
+              odit quis ut vel amet?
+            </div>
           </div>
         </div>
-        <div className="flip-card">
-          <div className="flip-card-inner">
-            <div className="flip-card-front">Mohammed Asim Ahmed</div>
-            <div className="flip-card-back">Mohammed Asim Ahmed</div>
-          </div>
-        </div>
-        <div className="flip-card">
-          <div className="flip-card-inner">
-            <div className="flip-card-front">Mohammed Asim Ahmed</div>
-            <div className="flip-card-back">Mohammed Asim Ahmed</div>
-          </div>
-        </div>
-      </div> */}
+      </motion.div>
     </div>
   );
 };
