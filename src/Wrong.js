d1dab695d95c6fdcb4140598900c05acb51c98f7
akshatbhansali <akshatb2198@gmail.com>
2024-03-22 18:57:37 +0530

    ada

---
+++ src/Wrong.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Wrong = () => {
+  return (
+    <div>
+      <button onClick={() => alert(`Answer is "Digital Evolution"`)}>
+        click
+        {/* sjvn */}
+      </button>
+    </div>
+  );
+};
+
+export default Wrong;
